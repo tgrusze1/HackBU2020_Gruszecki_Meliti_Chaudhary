dd17d5aef6bc53b7cae4f16e16f40474958d284b
Aryan2327 <achaudh9@binghamton.edu>
2020-02-08 14:53:16 -0500

    Web cam

--- GUI.py
+++ GUI.py
@@ -3,5 +3,19 @@ import numpy as np
 import cv2
 __all__ = [cv2, np, tkinter]
 
-vid = cv2.VideoCapture(-1, cv2.CAP_DSHOW)
+def create_webcam(mirror=False):
+    vid = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = vid.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+def main():
+    create_webcam(mirror = True)
+
+main()
 
