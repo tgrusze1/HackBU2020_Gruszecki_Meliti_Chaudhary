a8452254be6e6684334ecee0196d9b218daad756
Aryan2327 <achaudh9@binghamton.edu>
2020-02-08 15:00:41 -0500

    comments

--- GUI.py
+++ GUI.py
@@ -1,21 +1,27 @@
 import tkinter
 import numpy as np
 import cv2
+
 __all__ = [cv2, np, tkinter]
 
+
 def create_webcam(mirror=False):
-    vid = cv2.VideoCapture(0)
+    """
+    Opens camera.
+    """
+    vid = cv2.VideoCapture(0)  # Creates video object
     while True:
-        ret_val, img = vid.read()
-        if mirror:
+        ret_val, img = vid.read()  # Returns next frame
+        if mirror:  # Mirrors direction of movement in webcam
             img = cv2.flip(img, 1)
-        cv2.imshow('my webcam', img)
-        if cv2.waitKey(1) == 27:
+        cv2.imshow('my webcam', img)  # Opens webcam
+        if cv2.waitKey(1) == 27:  # Press escape to exit out of webcam.
             break  # esc to quit
     cv2.destroyAllWindows()
 
+
 def main():
-    create_webcam(mirror = True)
+    create_webcam(mirror=True)
+
 
 main()
-
