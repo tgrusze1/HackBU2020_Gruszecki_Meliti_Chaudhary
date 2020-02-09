7a497b9bf4b3a34bac2f922e95906f2c51a03bf0
Aryan2327 <achaudh9@binghamton.edu>
2020-02-09 00:02:35 -0500

    peeborg

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from PIL import Image, ImageTk
 from HackBU2020_Gruszecki_Meliti_Chaudhary import get_name
+from HackBU2020_Gruszecki_Meliti_Chaudhary import detect_body
 
 root = Tk()
 root.bind('<Escape>', lambda e: root.quit())
@@ -19,6 +20,21 @@ def activate_button():
     name = get_name.get_name(vid)
     name_in = Label(root, text=name)  # replace tdogg with function that takes the name
     name_in.place(y=930, x=50, relheight=.15, relwidth=.15)
+
+def activateclothing_button():
+    ret, frame = vid.read()
+    color_list = detect_body.detect_body(frame)
+    shirt_color = color_list[1]
+    pant_color = color_list[2]
+
+    top = Label(root, text=shirt_color)  # replace shirt with function that takes the top article
+    top.place(y=975, x=350 + 100, relheight=.07, relwidth=.07)
+
+    bot_in = Label(root, text=pant_color)  # replace pant with function that takes the bottom article
+    bot_in.place(y=975, x=550 + 200, relheight=.07, relwidth=.07)
+
+
+
 
 
 def get_frame():
@@ -41,10 +57,10 @@ def GUIWindow(width=1920, height=1080):
     root.resizable(width=False, height=False)
     root["bg"] = 'pink'
 
-    actbutton = Button(root, text="Activate", bg='green', command=activate_button)  # add command =  whatever it needs to do
+    actbutton = Button(root, text="Activate Name", bg='green', command=activate_button)  # add command =  whatever it needs to do
     actbutton.place(relx=0, rely=0, relheight=.1, relwidth=.1, anchor=NW)
-    debutton = Button(root, text="Deactivate", bg='red')  # add command =  whatever it needs to do
-    debutton.place(relx=0, y=100, relheight=.1, relwidth=.1, anchor=NW)
+    actclothing_button = Button(root, text="Activate Clothing", bg='green', command=activateclothing_button)  # add command =  whatever it needs to do
+    actclothing_button.place(relx=0, y=100, relheight=.1, relwidth=.1, anchor=NW)
 
     name_label = Label(root, text="Name")
     name_label.place(y=850, x=80, relheight=.1, relwidth=.1)
@@ -52,14 +68,8 @@ def GUIWindow(width=1920, height=1080):
     top = Label(root, text="Top")
     top.place(y=865, x=345 + 100, relheight=.07, relwidth=.07)
 
-    top = Label(root, text="Shirt")  # replace shirt with function that takes the top article
-    top.place(y=975, x=350 + 100, relheight=.07, relwidth=.07)
-
     bot = Label(root, text="Bottom")
     bot.place(y=865, x=545 + 200, relheight=.07, relwidth=.07)
-
-    bot_in = Label(root, text="Pants")  # replace shirt with function that takes the bottom article
-    bot_in.place(y=975, x=550 + 200, relheight=.07, relwidth=.07)
 
     shoe = Label(root, text="Footwear")
     shoe.place(y=865, x=745 + 300, relheight=.07, relwidth=.07)
